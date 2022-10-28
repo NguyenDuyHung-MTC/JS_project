853a7d9ea6d9af4396a67b99d9ed4ebf904fa667
Nguyễn Duy Hùng <116874003+NguyenDuyHung-MTC@users.noreply.github.com>
2022-10-28 21:04:05 +0700

    Add files via upload

---
+++ js-tabs/main.js
@@ -0,0 +1,19 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document) 
+
+const tabs = $$('.tab-item')
+const panes = $$('.tab-pane')
+const line = $('.line')
+
+tabs.forEach((tab, index) => {
+    const pane = panes[index]
+    tab.onclick = function(){
+        $('.tab-item.active').classList.remove('active')
+        $('.tab-pane.active').classList.remove('active')
+        line.style.left = tab.offsetLeft + 'px'
+        line.style.width = tab.offsetWidth + 'px'
+
+        tab.classList.add('active')
+        pane.classList.add('active')
+    }
+})
